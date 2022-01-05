4e09acc7b83aacbbccbd28865f776064ac25935e
andrewyoder <andrwydr@aol.com>
2022-01-05 13:58:58 -0800

    Add DOM elements to play on page, rather than console

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # rock-paper-scissors
 A rock paper scissors game in JavaScript
 
-The player's move is entered via an on-screen prompt and the computer's move is determined randomly.
+The player's move is entered via button and the computer's move is determined randomly.
 
-Results are shown in the developer console.
+Results are shown on-screen utilizing DOM manipulation.
 
-The game is best 3 out of 5 rounds.
+The winner is the first to score 5 points.
--- game.js
+++ game.js
@@ -1,8 +1,127 @@
-const rock = 0;
-const paper = 1;
-const scissors = 2;
+/**
+ * Add button listeners to capture gameplay; record result
+ * and update score displayed
+ */
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    let result = playRound(e.target.id);
+    if (result === "DRAW") return;
+    if (result === "WIN") {
+      playerScore.textContent++;
+    }
+    if (result === "LOSE") {
+      computerScore.textContent++;
+    }
 
+    // first to reach 5 wins, so add a win/lose statement and reset scores
+    if (+playerScore.textContent == 5 || +computerScore.textContent == 5) {
+      const container = document.querySelector('body');
+
+      // win/lose and final score statement
+      const finalScore = document.createElement('div');
+      finalScore.textContent = `You ${result}! Final Score: `
+          + `${playerScore.textContent} to ${computerScore.textContent}`;
+      finalScore.style.cssText = 'text-align: center';
+
+      container.appendChild(finalScore);
+
+      // reset scores for next game
+      playerScore.textContent = 0;
+      computerScore.textContent = 0;
+    }
+  });
+});
+
+/**
+ * play a round of rock, paper, scissors
+ * 
+ * @param {string} playerMove
+ * @returns {string} result of the round
+ */
+function playRound(playerMove) {
+  let computerMove = computerPlay();
+
+  if (playerMove === computerMove) {
+    console.log("It's a Draw!");
+    return("DRAW");
+  }
+  
+  if (playerMove === 'ROCK' && computerMove === 'SCISSORS' ||
+      playerMove === 'PAPER' && computerMove === 'ROCK' ||
+      playerMove === 'SCISSORS' && computerMove === 'PAPER') {
+    console.log(`You Win! ${playerMove} beats ${computerMove}!`);
+    return("WIN");
+  }
+  console.log(`You Lose! ${computerMove} beats ${playerMove}!`);
+  return("LOSE");
+}
+
+
+/**
+ * select random move for computer
+ * 
+ * @returns {string} rock, paper, or scissors
+ */
+function computerPlay() {
+  let move = Math.floor(Math.random()*3);
+  
+  return getMoveName(move);
+}
+
+/**
+ * helper function for computerPlay()
+ * 
+ * @param {number} move
+ * @returns {string} rock, paper, or scissors
+ */
+function getMoveName(move) {
+  if (move === 0) {
+    return "ROCK";
+  } else if (move === 1) {
+    return "PAPER";
+  } else {
+    return "SCISSORS";
+  }
+}
+
+
+
+
+/* ------------------------------------------------------------------------- */
+
+/* DEPRECATED -- game plays when buttons are pressed */
+/*
+game();
+*/
+
+
+/* DEPRECATED -- player move now determined by button selection */
+/* use prompt() to get the player's move */
+/*
+function getPlayerMove() {
+  do {
+    let move = prompt("What's your move?");
+
+    if (move.toLowerCase() === "rock") {
+      return rock;
+    } else if (move.toLowerCase() === "paper") {
+      return paper;
+    } else if (move.toLowerCase() === "scissors") {
+      return scissors;
+    } else {
+      console.log("You entered an invalid response. Please enter 'Rock', 'Paper', or 'Scissors'");
+    }
+  } while (true);
+}
+*/
+
+
+/* DEPRECATED -- for console gameplay */
 /* play 5 rounds, keep score, and report the winner */
+/*
 function game() {
   let playerScore = 0;
   let computerScore = 0;
@@ -29,59 +148,4 @@ function game() {
                                                 `You lose! ${playerScore} - ${computerScore}`;
   console.log(message);
 }
-
-/* play a round of rock, paper, scissors */
-function playRound(playerMove, computerMove) {
-  console.log("...Rock!\n...Paper!\n...Scissors!\nShoot!\n");
-
-  if (playerMove == computerMove) {
-    console.log("It's a Draw!");
-    return("DRAW");
-  }
-  
-  if (playerMove == rock && computerMove == scissors ||
-      playerMove == paper && computerMove == rock ||
-      playerMove == scissors && computerMove == paper) {
-    console.log(`You Win! ${getMoveName(playerMove)} beats ${getMoveName(computerMove)}!`);
-    return("WIN");
-  }
-  console.log(`You Lose! ${getMoveName(computerMove)} beats ${getMoveName(playerMove)}!`);
-  return("LOSE");
-}
-
-/* use prompt() to get the player's move */
-function getPlayerMove() {
-  do {
-    let move = prompt("What's your move?");
-
-    if (move.toLowerCase() === "rock") {
-      return rock;
-    } else if (move.toLowerCase() === "paper") {
-      return paper;
-    } else if (move.toLowerCase() === "scissors") {
-      return scissors;
-    } else {
-      console.log("You entered an invalid response. Please enter 'Rock', 'Paper', or 'Scissors'");
-    }
-  } while (true);
-}
-
-/* select random move for computer */
-function computerPlay() {
-  let move = Math.floor(Math.random()*3);
-  
-  return move;
-}
-
-/* return the English name of the move's numerical value */
-function getMoveName(move) {
-  if (move == rock) {
-    return "ROCK";
-  } else if (move == paper) {
-    return "PAPER";
-  } else {
-    return "SCISSORS";
-  }
-}
-
-game();
+*/
